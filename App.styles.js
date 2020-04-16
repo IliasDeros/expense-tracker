3946746690e58f96ec58ac6fcaf2aaacf53f568f
Ilias <ideros21@gmail.com>
2020-04-15 21:25:15 -0400

    Add colors to buttons

--- App.styles.js
+++ App.styles.js
@@ -2,7 +2,9 @@ import { StyleSheet } from 'react-native';
 
 export const colors = {
   partnerRed: '#8D021F',
+  partnerRedBright: '#ED1C35',
   selfGreen: '#2EBB57',
+  selfGreenBright: '#3BF570',
 }
 
 export const styles = StyleSheet.create({
--- components/ExpenseSubmit/index.js
+++ components/ExpenseSubmit/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { StyledButton, Container } from './styles'
+import { colors } from '../../App.styles'
 
 export function ExpenseSubmit({ submitPartner, submitSelf }) {
   return (
     <Container>
       <StyledButton 
         title="You paid"
+        color={colors.selfGreenBright}
         onPress={submitSelf}
       />
       <StyledButton 
         title="They paid"
+        color={colors.partnerRedBright}
         onPress={submitPartner}
       />
     </Container>
